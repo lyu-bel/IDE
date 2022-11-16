b8e1e520b96f52500b9a0e1acc8bcc817868ecb5
lyu-bel <lyu-bel@yandex.ru>
2022-11-16 23:15:39 +0700

    Initial commit

---
+++ gg.py
@@ -0,0 +1,38 @@
+def func():
+  #guess the number РАБОЧАЯ ВЕРСИЯ проверка на 1000 проходов
+  import numpy as np
+  n = 100
+  number = np.random.randint(1, n+1) # загадываем число
+  count=1
+  predict_number=n/2
+  if number>50:
+    nd=50
+    kd=101
+  if number<50:
+    nd=0
+    kd=50
+  while True:
+    if predict_number==number:
+      if count>=20:
+        flag=0
+      #print(f"Вы угадали число! Это число = {number}, за {count} попыток") 
+      break  
+    while predict_number<number:
+      ub=predict_number
+      predict_number=(nd+kd)//2
+      nd=predict_number
+      count +=1
+      if predict_number>number:
+        nd=ub
+        kd=predict_number
+        break
+    while predict_number>number:
+      ub=predict_number
+      predict_number=(nd+kd)//2
+      kd=predict_number
+      count +=1
+      if predict_number<number:
+        nd=predict_number
+        kd=ub
+        break
+  return(count)
